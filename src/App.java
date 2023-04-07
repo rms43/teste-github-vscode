d8c1537c9b040020caf6828c666816df3ea14e7d
rms43 <rafa.malassise@gmail.com>
2023-04-07 05:41:46 -0300

    refactor: alter App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá Mundo!");
+        JOptionPane.showMessageDialog(null, "Rafal M Santos");
     }
 }
